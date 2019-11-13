0048046ebbbbb173f2023da367e67b6b502e8d9b
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-12 23:04:59 -0300

    cadastro de matricula e validaçoes

--- src/app/controllers/MatriculationController.js
+++ src/app/controllers/MatriculationController.js
@@ -1,5 +1,5 @@
-import * as Yup from 'yup';
-import { Op } from 'sequelize';
+import { addMonths, parseISO, isBefore } from 'date-fns';
+
 import Matriculation from '../models/Matriculation';
 import Student from '../models/Student';
 import Plan from '../models/Plan';
@@ -10,14 +10,45 @@ class MatriculationController {
     }
 
     async store(req, res) {
-        const { student_id, start_date, plan_id, end_date, price } = req.body;
+        const { student_id, start_date, plan_id } = req.body;
+
+        if (isBefore(parseISO(start_date), new Date())) {
+            return res.status(400).json({
+                error: `past dates dont permited today is ${new Date()}`,
+            });
+        }
+
+        const student = await Student.findByPk(student_id);
+
+        if (!student) {
+            return res.status(404).json({ error: 'Student does not exist' });
+        }
+
+        const studentMatriculationExist = await Matriculation.findOne({
+            where: { student_id: student.id },
+        });
+
+        if (studentMatriculationExist) {
+            return res
+                .status(400)
+                .json({ error: `${student.name} already enrolled` });
+        }
+
+        const plan = await Plan.findByPk(plan_id);
+
+        if (!plan) {
+            return res.status(404).json({ error: 'plan does not exist' });
+        }
+
+        const price = plan.price * plan.duration;
+        const end_date = addMonths(parseISO(start_date), plan.duration);
 
         const matriculation = await Matriculation.create({
             student_id,
             plan_id,
             start_date,
-            end_date,
             price,
+            end_date,
         });
 
         return res.json(matriculation);
