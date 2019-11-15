2d9b946f5b1c2d35c464f917a1fbd581e5360202
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-15 14:32:00 -0300

    conta e lista o chekings

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,21 +1,67 @@
+import { isWithinInterval, subDays } from 'date-fns';
+import { Op } from 'sequelize';
 import Checkin from '../models/Checkin';
+import Subscription from '../models/Subscription';
 
 class CheckinController {
-    async index(req, res) {
-        return res.json({});
-    }
+  async store(req, res) {
+    const student_id = req.params.id;
 
-    async store(req, res) {
-        return res.json({});
+    const subscription = await Subscription.findOne({
+      where: {
+        student_id
+      }
+    });
+     ​
+    if (!subscription) {
+      return res.status(404).json({ error: 'Subscription not found' });
     }
-
-    async update(req, res) {
-        return res.json({});
+​
+    const subscritprionInterval = {
+      start: subscription.start_date,
+      end: subscription.end_date
+    };
+​
+    if (!isWithinInterval(new Date(), subscritprionInterval)) {
+      return res.status(404).json({ error: 'Subscription not active.' });
     }
-
-    async delete(req, res) {
-        return res.json({});
+​
+    const sevenDaysAgo = subDays(new Date(), 7);
+​
+    const checkins = await Checkin.findAndCountAll({
+      where: {
+        student_id,
+        created_at: {
+          [Op.gte]: sevenDaysAgo
+        }
+      }
+    });
+​
+    if (checkins.count > 5) {
+      return res.status(401).json({
+        error: 'You have more than 5 checkins this week.',
+        checkins: checkins.rows
+      });
     }
+​
+    const checkin = await Checkin.create({
+      student_id
+    });
+​
+    return res.json(checkin);
+  }
+​
+  async index(req, res) {
+    const student_id = req.params.id;
+​
+    const checkinList = await Checkin.findAll({
+      where: {
+        student_id
+      }
+    });
+​
+    return res.json(checkinList);
+  }
 }
-
+​
 export default new CheckinController();
